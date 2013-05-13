24f1a59fe01ae14e41a52aaa604008d9df4d3c84
j.constantine <platinumgulls@outlook.com>
2013-05-13 16:10:21 -0500

    Added the folder with all the files to the repo

---
+++ demo/list-sort.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+var $mtv = jQuery.noConflict();
+$mtv(document).ready(listSort);
+
+function listSort(){
+$mtv('div.tags').delegate('input[type=checkbox]', 'change', function()
+{
+    var lis = $mtv('.results > li'),
+        checked = $mtv('input:checked');
+
+    if (checked.length)
+    {
+        var selector = checked.map(function ()
+        {
+            return '.' + $mtv(this).attr('rel');
+        }).get().join();
+
+        lis.hide().filter(selector).show().addClass("show");   
+        console.log(selector)
+    }
+    else
+    {
+        lis.show().removeClass("show");
+    }
+});
+
+
+};
+
+
